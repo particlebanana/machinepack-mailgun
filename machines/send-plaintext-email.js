7d7e902acbe067847202e978a3b067851e6aaea0
Mike McNeil <mike@balderdash.co>
2015-01-07 23:55:42 -0800

    All inputs work for send-plaintext-email machine.

--- machines/send-plaintext-email.js
+++ machines/send-plaintext-email.js
@@ -14,9 +14,36 @@ module.exports = {
       }
     },
     toEmail: {
-      description: 'Email address of the primary recipient.',
+      friendlyName: 'To (email)',
       example: 'jane@example.com',
+      description: 'Email address of the primary recipient.',
       required: true
+    },
+    toName: {
+      friendlyName: 'To (name)',
+      example: 'Jane Doe',
+      description: 'Full name of the primary recipient.',
+      extendedDescription: 'If left blank, defaults to the recipient\'s email address.'
+    },
+    subject: {
+      friendlyName: 'Subject',
+      description: 'Subject line for the email.',
+      example: 'Welcome, Jane!'
+    },
+    message: {
+      friendlyName: 'Message',
+      description: 'The plaintext body of the email.',
+      example: 'Jane,\nThanks for joining our community.  If you have any questions, please don\'t hesitate to send them our way.  Feel free to reply to this email directly.\n\nSincerely,\nThe Management'
+    },
+    fromEmail: {
+      friendlyName: 'From (email)',
+      description: 'Email address of the sender.',
+      example: 'harold@example.enterprise'
+    },
+    fromName: {
+      friendlyName: 'From (name)',
+      description: 'Full name of the sender.',
+      example: 'Harold Greaseworthy'
     }
   },
   defaultExit: 'success',
@@ -30,9 +57,33 @@ module.exports = {
   },
   fn: function(inputs, exits) {
 
-    var mg = new Mailgun('api-key');
+    var util = require('util');
+    var Mailgun = require('mailgun').Mailgun;
 
-    mg.sendText('foo@bar.com', [inputs.toEmail], 'subject', 'the text', '', {}, function (err){
+    var mg = new Mailgun(inputs.apiKey);
+
+    // e.g. ['John Doe <john@example.com>']
+    var recipients = [
+      (function(){
+        if (!inputs.toName) {
+          return inputs.toEmail;
+        }
+        return util.format('%s <%s>',inputs.toName, inputs.toEmail);
+      })()
+    ];
+
+    // e.g. 'John Doe <john@example.com>'
+    var from = (function(){
+      if (!inputs.fromEmail){
+        return 'noreply@example.com';
+      }
+      if (!inputs.fromName) {
+        return inputs.fromEmail;
+      }
+      return util.format('%s <%s>',inputs.fromName, inputs.fromEmail);
+    })();
+
+    mg.sendText(from, recipients, inputs.subject||'Hello world!', inputs.message||' ', '', {}, function (err){
       if (err) return exits.error(err);
       return exits.success();
     });
